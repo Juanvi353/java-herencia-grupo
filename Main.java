f3a190206887c5bbef5ff9181445e15215a0118d
pgc2003 <pgarcer2003@gmail.com>
2026-01-20 14:19:31 +0100

    Main_update

--- Main.java
+++ Main.java
@@ -1,21 +1,24 @@
+package herencia;
+
 import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        Empleado empleado1 = new Empleado("Antonio Lobato", 3000.0);
-        Desarrollador dev1 = new Desarrollador("Mariano Rajoy", 4000.0, "Java");
-        Manager manager1 = new Manager("Mark Verstappen", 5000.0, 5);
+
+        Empleado e1 = new Empleado("123A", "Antonio Lobato", 45, 3000, 5);
+        Desarrollador d1 = new Desarrollador("456B", "Mariano Rajoy", 60, 4000, 10, "Java");
+        Manager m1 = new Manager("789C", "Max Verstappen", 26, 5000, 3, 5);
 
         ArrayList<Empleado> empleados = new ArrayList<>();
-        empleados.add(empleado1);
-        empleados.add(dev1);
-        empleados.add(manager1);
+        empleados.add(e1);
+        empleados.add(d1);
+        empleados.add(m1);
 
         for (Empleado emp : empleados) {
-            System.out.println("Tipo real del objeto: " + emp.getClass().getSimpleName());
-            System.out.println("Presentación: " + emp.presentarse());
-            System.out.println("Salario calculado: " + emp.calcularSalario());
-            System.out.println("---");
+            System.out.println("Tipo real: " + emp.getClass().getSimpleName());
+            System.out.println(emp.presentarse());
+            System.out.println("Salario: " + emp.calcularSalario());
+            System.out.println("-----");
         }
     }
-} 
+}
