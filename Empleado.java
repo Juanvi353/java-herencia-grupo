db36e5c316daa55b882f095ebd14e0fd7e3032a2
AndreaContreras06 <andreacontreraslopez06@gmail.com>
2026-01-27 14:35:14 +0100

    departamento

--- Empleado.java
+++ Empleado.java
@@ -1,9 +1,18 @@
 package herencia;
 
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
 public class Empleado extends Persona {
+
 
     protected double salarioBase;
     protected int antiguedadAnios;
+    protected String idEmpleado;
+    protected LocalDate fechaContratacion;
+    protected String departamento;
+    protected double jornadaHoraria;
+    protected boolean activo;
 
     public Empleado(String dni, String nombre, int edad,
                     double salarioBase, int antiguedadAnios) {
@@ -12,8 +21,16 @@ public class Empleado extends Persona {
         this.antiguedadAnios = antiguedadAnios;
     }
 
+    public int calcularAntiguedad() {
+        if (fechaContratacion == null) {
+            return antiguedadAnios; 
+        }
+        return (int) ChronoUnit.YEARS.between(fechaContratacion, LocalDate.now());
+    }
+
     public double calcularSalario() {
-        return salarioBase + (antiguedadAnios * 100);
+        int anios = calcularAntiguedad();
+        return salarioBase * (1 + (anios * 0.03));
     }
 
     @Override
